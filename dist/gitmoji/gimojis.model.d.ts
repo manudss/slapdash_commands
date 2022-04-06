df8767d8076ce7fd538e361c62b8fe23b382de9a
Emmanuel DE SAINT STEBAN <emmanuel.desaintsteban@evidian.com>
2022-04-06 15:45:08 +0200

    :construction_worker: remove dist directory

--- dist/gitmoji/gimojis.model.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Gitmojis {
-    emoji: string;
-    entity: string;
-    code: string;
-    description: string;
-    name: string;
-    semver?: any;
-}
-export declare type CopyType = 'emoji' | 'code' | 'description' | 'name' | 'entity';
--- dist/gitmoji/gitmoji.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CommandResponse } from '@slapdash/command-response-types';
-import { CopyType } from './gimojis.model';
-import { GitmojiService } from './gitmoji.service';
-export declare class GitmojiController {
-    private gitmojiService;
-    constructor(gitmojiService: GitmojiService);
-    getGitmoji(copyType: CopyType, selected: string): CommandResponse;
-}
